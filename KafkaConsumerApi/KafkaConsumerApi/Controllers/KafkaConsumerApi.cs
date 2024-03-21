727b23dc0f96e34c7fa262813a20ed6411771e56
imadett88 <imadettamen18@gmail.com>
2024-03-21 10:15:26 +0100

    FC

---
+++ KafkaConsumerApi/KafkaConsumerApi/Controllers/KafkaConsumerApi.cs
@@ -0,0 +1,49 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading;
+
+namespace KafkaConsumerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KafkaConsumerController : ControllerBase
+    {
+        private readonly ConsumerConfig _config;
+
+        public KafkaConsumerController()
+        {
+            _config = new ConsumerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                GroupId = "demo_group",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+        }
+
+        [HttpGet]
+        public IActionResult ConsumeMessage()
+        {
+            using (var consumer = new ConsumerBuilder<Ignore, string>(_config).Build())
+            {
+                consumer.Subscribe("demo");
+
+                while (true)
+                {
+                    try
+                    {
+                        var message = consumer.Consume(TimeSpan.FromSeconds(1));
+                        if (message != null)
+                        {
+                            return Ok(message.Message.Value);
+                        }
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occurred: {e.Error.Reason}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ KafkaProducerApi/KafkaProducerApi/Controllers/KafkaProducerController.cs
@@ -0,0 +1,32 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KafkaProducerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KafkaProducerController : ControllerBase
+    {
+        private readonly ProducerConfig _config;
+
+
+        public KafkaProducerController()
+        {
+            _config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092"
+            };
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProduceMessage([FromBody] string msg)
+        {
+            using (var producer = new ProducerBuilder<Null, string>(_config).Build())
+            {
+                await producer.ProduceAsync("demo",new Message<Null, string> { Value = msg });
+                return Ok();
+            }
+        }
+    }
+}
